ac894a22bbf703835d075e45757eb79ba69c6018
sukmaranggapradeta <adit.netral@gmail.com>
2019-07-26 21:18:05 +0700

    room detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router , Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import RoomPage from './pages/Room/Room'
+import RoomPageDetail from './pages/Room/RoomDetail'
 import HomePage from './pages/Home/Home'
 
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
             {/* <PrivateRoute exact path="/" component={ Home } /> */}
             <Route exact path="/" component={ HomePage } />
             <Route exact path='/room' component={ RoomPage } />
+            <Route exact path='/room/:id' component={ RoomPageDetail } />
           </Switch>
         </Router>
       </Provider>
--- src/pages/Room/RoomCard.js
+++ src/pages/Room/RoomCard.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 import './Room.css';
 
 export class RoomList extends Component {
     render() {
         return (
             <div className="col s12 m4">
-                <div className="card-panel  grey darken-3 hoverable ">
-                <span className="white-text">Player One</span><br/>
-                <span className="white-text">Player Two</span><br/>
+                <div className="card blue-grey darken-1 hoverable">
+                    <div className="card-content white-text">
+                    <span className="white-text">Player One</span><br/>
+                    <span className="white-text">Player Two</span><br/>
+                    </div>
+                    <div className="card-action">
+                    <Link to="/room/id">Join Room</Link>
+                    </div>
                 </div>
             </div>
         )
