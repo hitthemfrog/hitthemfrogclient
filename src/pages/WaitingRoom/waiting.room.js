511ecc0fa8a484bb234e88727828178f5d885c7e
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-29 00:53:18 +0700

    subscribe to rooms in waiting room component

--- src/pages/WaitingRoom/waiting.room.js
+++ src/pages/WaitingRoom/waiting.room.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import Loading from '../../component/Loading'
+import { connect } from 'react-redux'
 
-export default class extends React.Component {
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
-  render () {
-    return <Loading/>
+const mapStateToProps = state => {
+  return {
+    rooms: state.rooms
   }
 }
+export default connect(mapStateToProps)(
+  class extends React.Component {
+    checkPlayers() {
+      let roomname = localStorage.getItem('htf_roomname')
+      let room = this.props.rooms.find(e => e.name === roomname)
+      if (room && room.players.length === 2) {
+        this.props.history.push('/game')
+      }
+    }
+    
+    componentDidMount() {
+      this.checkPlayers()
+    }
+
+    componentDidUpdate() {
+      this.checkPlayers()  
+    }
+
+    render () {
+      return <Loading/>
+    }
+  }
+)
