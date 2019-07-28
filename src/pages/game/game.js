29674cf28a69d4e8591a67cba3afe82e847d45af
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-28 23:12:31 +0700

    remove duplicate socket in game.js

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -4,9 +4,6 @@ import initModels from "./hitthefrog/init.models";
 import { mouseClickListener, mouseMoveListener } from './hitthefrog/events'
 import loop from './hitthefrog/loop'
 import initHud from './hitthefrog/hud'
-import io from 'socket.io-client';
-
-const socket = io('http://localhost:3000');
 
 let playerDataListener = function (playerList) {
   console.log(playerList)
@@ -18,16 +15,16 @@ class Game extends Component {
     let { sceneHUD, setScoreHUD, cameraHUD } = initHud(this.gameThree)
     let models = await initModels(setup.scene)
     let clickedObjUUidArray = []
-    mouseClickListener(setup.camera, setup.scene, socket, models)
+    mouseClickListener(setup.camera, setup.scene, this.props.socket, models)
     mouseMoveListener(setup.camera, setup.scene, clickedObjUUidArray)
-    loop(setup.scene, setup.renderer, setup.camera, sceneHUD, cameraHUD, setScoreHUD, socket)
+    loop(setup.scene, setup.renderer, setup.camera, sceneHUD, cameraHUD, setScoreHUD, this.props.socket)
 
-    socket.on('playersData', playerDataListener)
+    this.props.socket.on('playersData', playerDataListener)
     
   }
 
   componentWillUnmount() {
-    socket.removeListener('playersData', playerDataListener)
+    this.props.socket.removeListener('playersData', playerDataListener)
   }
 
   render() {
