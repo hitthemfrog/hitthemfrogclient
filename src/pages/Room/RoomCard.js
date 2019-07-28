763a721de5b2dba35b7e5e943c256d917d655472
sukmaranggapradeta <adit.netral@gmail.com>
2019-07-28 10:35:11 +0700

    layout home room detail

--- src/pages/Room/RoomCard.js
+++ src/pages/Room/RoomCard.js
@@ -4,24 +4,35 @@ import { Link } from 'react-router-dom'
 import './Room.css';
 
 export class RoomList extends Component {
+    state = {
+        redirectToRoomDetail : false
+    }
+    joinPlayerToRoom() {
+
+    }
+
     render() {
         return (
-            <div className="col s12 m4">
+            <div className="col s12 m3">
                 <div className="card blue-grey darken-1 hoverable">
                     <div className="card-content white-text">
                     {/* <span className="white-text">{JSON.stringify(this.props.data)}</span><br/> */}
                     <h3 className="white-text">{ this.props.data.name }</h3><br/>
-                    <h6 className="white-text">{ this.props.data.status }</h6><br/>
                     {   
                         this.props.data.players.map((playerInRoom, i) => 
-                            <span className="white-text" key={i}>{ playerInRoom.name }</span>
+                        <>
+                        <span className="white-text" key={i}>{ playerInRoom.playerName }</span><br></br>
+                        </>
                         )
                     }
-                    {/* <span className="white-text">{ this.props.data.players[0].playerName }</span><br/>
-                    <span className="white-text">{ this.props.data.players[1].playerName }</span><br/> */}
+                    <span className="white-text">{ this.props.data.status } FOR YOU</span><br/>
                     </div>
                     <div className="card-action">
-                    <Link to="/room/id">Join Room</Link>
+                        {
+                            (this.props.data.status === 'waiting')
+                            &&
+                            <Link to={ `/room/${this.props.data.name}` }>Join Room</Link>
+                        }
                     </div>
                 </div>
             </div>
