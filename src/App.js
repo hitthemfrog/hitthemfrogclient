ac1509859fd7b23ad1ee6b216efbbec6fed0a08a
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-28 23:12:45 +0700

    refactor inline functioin in render route component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import './App.css';
 import React from 'react';
 import { BrowserRouter as Router , Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import io from 'socket.io-client';
 import store from './store'
-import './App.css';
+
 import RoomPage from './pages/Room/Room'
 import RoomPageDetail from './pages/Room/RoomDetail'
 import HomePage from './pages/Home/Home'
 import WaitingRoom from './pages/WaitingRoom/waiting.room' 
-import io from 'socket.io-client';
 import Game from './pages/game/game'
 
 const socket = io('http://localhost:3000');
 
+const renderWithSocket = Component => (props) => (
+  <Component {...props} socket={socket} />
+)
 
 function App() {
   return (
@@ -19,14 +23,11 @@ function App() {
       <Provider store={ store } >
         <Router>
           <Switch>
-            {/* <PrivateRoute exact path="/" component={ Home } /> */}
-            <Route exact path="/" component={ HomePage  } />
-            <Route exact path='/room' render={(props) => <RoomPage {...props} socket={socket} />} />            
-            <Route exact path='/waitingRoom' render={(props) => <WaitingRoom {...props} socket={socket} />} />            
-            {/* <Route exact path='/room' component={ RoomPage } /> */}
-            <Route exact path='/game' component={Game} />
-            <Route exact path='/room/:roomname' render={(props) => <RoomPageDetail {...props} socket={socket} />} />            
-            {/* <Route exact path='/room/:roomname' component={ RoomPageDetail } /> */}
+            <Route exact path="/" component={ HomePage } />
+            <Route exact path='/room' render={renderWithSocket(RoomPage)} />} />            
+            <Route exact path='/waitingRoom' render={renderWithSocket(WaitingRoom)} />            
+            <Route exact path='/game' render={renderWithSocket(Game)} />
+            <Route exact path='/room/:roomname' render={renderWithSocket(RoomPageDetail)} />            
           </Switch>
         </Router>
       </Provider>
