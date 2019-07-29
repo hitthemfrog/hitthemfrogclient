deb80d422a6551bf39fa78411d7c5050002bd4f9
Isnan Irsyad <sao.irsyad@gmail.com>
2019-07-29 16:40:02 +0700

    Re-layout for game over screen

--- src/pages/gameOverScreen/gameOverScreen.js
+++ src/pages/gameOverScreen/gameOverScreen.js
@@ -8,10 +8,37 @@ const mapStateToProps = state => {
 }
 
 function gameover (props) {
+  const username = localStorage.getItem('htf_username')
+  const isWinner = username === props.isGameFinished.winner
+
+  if (!props.isGameFinished.score) return props.history.push('/room')
+
   return (
-    <h1>
-      { JSON.stringify(props.isGameFinished) }
-    </h1>
+  <div id="style-15" className="roomBox scrollbar force-overflow">
+    <div className='row'>
+      <div className='col s12 m12 l12'>
+        { isWinner &&
+          <div>
+            <h1><i>Congratulations!!, </i>{ username }</h1>
+            
+          </div>
+        }
+        {
+          !isWinner &&
+          <h1><i>You Lose!!, </i>{ username } !!</h1>
+        }
+        
+        <hr />
+        <h3>Score: { props.isGameFinished.score[username] }</h3>
+
+        <button className="btnnya-main linkStyle" onClick={ () => {
+          localStorage.removeItem('htf_roomname')
+          props.history.push('/room')
+        }}>AYO MAIN LAGI</button>
+
+      </div>
+    </div>      
+  </div>
   )
 }  
 
