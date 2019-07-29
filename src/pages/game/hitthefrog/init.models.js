44052c35e2ee5c839d7882dc2b69cea0789cafc2
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-29 19:29:11 +0700

    add prefix when loading the model

--- src/pages/game/hitthefrog/init.models.js
+++ src/pages/game/hitthefrog/init.models.js
@@ -61,8 +61,10 @@ function randomCoordinate() {
   return result
 }
 
+const prefix = (process.env.NODE_ENV === 'production') ? '/hitthemfrogclient' : ''
+
 export default async function (scene) {
-  const frogModel = '/models/simple.frog.glb'
+  const frogModel = prefix + '/models/simple.frog.glb'
   const monkeyModel = '/models/simple.frog.sad.glb'
   let frogs = []
   let frogObj = store.getState().frogs
