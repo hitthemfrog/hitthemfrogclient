eaf176469ab896e7327b81bbd30775da19f0d031
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-29 19:21:16 +0700

    fix host refer to local when building the app

--- src/host.js
+++ src/host.js
@@ -1,9 +1,9 @@
 let SOCKET_IO_HOST = ''
 
-if (process.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   SOCKET_IO_HOST = 'http://localhost:3333'
 } else {
-  SOCKET_IO_HOST = 'http://34.87.30.146'
+  SOCKET_IO_HOST = 'https://hitthemfrogsocket.khariskhasburrahman.com'
 }
 
 export default SOCKET_IO_HOST
