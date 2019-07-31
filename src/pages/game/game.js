42cb2a91204714763e17f50296466799ef2eb151
sukmaranggapradeta <adit.netral@gmail.com>
2019-07-31 14:39:57 +0700

    setup router navigasi gameover and game

--- src/pages/game/game.js
+++ src/pages/game/game.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
-import sceneSetup from "./hitthefrog/scene.setup";
 import initModels from "./hitthefrog/init.models";
+import sceneSetup from "./hitthefrog/scene.setup";
 import { mouseClickListener, mouseMoveListener } from './hitthefrog/events'
 import loop from './hitthefrog/loop'
 import initHud from './hitthefrog/hud'
 import gameSoundMini from '../../sound/gameSoundMini.mp3'
+// import { Redirect } from 'react-router-dom'
 
 class Game extends Component {
+  
   async componentDidMount() {
-    let setup = sceneSetup(this.gameThree)
-    let { sceneHUD, setScoreHUD, cameraHUD } = initHud(this.gameThree)
-    let models = await initModels(setup.scene)
-    let clickedObjUUidArray = []
-    mouseClickListener(setup.camera, setup.scene, this.props.socket, models)
-    mouseMoveListener(setup.camera, setup.scene, clickedObjUUidArray)
-    loop(setup.scene, setup.renderer, setup.camera, sceneHUD, cameraHUD, setScoreHUD, this.props.socket, this.props.history)
+  
+    if (!localStorage.getItem('htf_roomname')){
+      this.props.history.push('/room')
+      return null  
+    } else {
+      let setup = sceneSetup(this.gameThree)
+      let { sceneHUD, setScoreHUD, cameraHUD } = initHud(this.gameThree)
+      let models = await initModels(setup.scene)
+      let clickedObjUUidArray = []
+      mouseClickListener(setup.camera, setup.scene, this.props.socket, models)
+      mouseMoveListener(setup.camera, setup.scene, clickedObjUUidArray)
+      loop(setup.scene, setup.renderer, setup.camera, sceneHUD, cameraHUD, setScoreHUD, this.props.socket, this.props.history)
+    }
   }
 
   render() {
@@ -23,7 +31,11 @@ class Game extends Component {
       <div>
           <audio src={gameSoundMini} autoPlay loop/>
       </div>
-      <div ref={ref => (this.gameThree = ref)} />
+        <div ref={ref => (this.gameThree = ref)} />
+      {/* {
+        !localStorage.getItem('htf_roomname') &&
+        <Redirect to="/room"/>
+      } */}
       </>
     )
   }
--- src/pages/gameOverScreen/gameOverScreen.js
+++ src/pages/gameOverScreen/gameOverScreen.js
@@ -19,57 +19,62 @@ const mapDispatchToProps = dispatch => {
 
 
 function Gameover (props) {
-
-  const username = localStorage.getItem('htf_username');
-  let scores = Object.values(props.isGameFinished.score)
-  scores = scores.reduce((acc, curr) => acc - curr)
-  const isDraw = scores === 0
-  const isWinner = username === props.isGameFinished.winner;
+  console.log(props, " ini props")
 
   useEffect(() => {
     props.socket.emit('leaveRoom', localStorage.getItem('htf_roomname') );
     localStorage.removeItem('htf_roomname');
-
+    
     return () => {
       props.clearGame()
     }
   }, [])
-
-  if (!props.isGameFinished.score) return props.history.push('/room');
-
-
-  return (
-  <div id="style-15" className="roomBox scrollbar force-overflow">
-    <div className='row'>
-      <div className='col s12 m12 l12'>
-        { isWinner && !isDraw &&
-          <div>
-            <h1 className="playerNameStyle"><i>Congratulations!!, </i>{ username }</h1>
+  
+  if (props.isGameFinished.score === undefined) {
+    props.history.push('/room')
+    return null
+  } else {
+    const username = localStorage.getItem('htf_username');
+    let scores = Object.values(props.isGameFinished.score)
+    scores = scores.reduce((acc, curr) => acc - curr)
+    const isDraw = scores === 0
+    const isWinner = username === props.isGameFinished.winner;
+    return (
+      <div id="style-15" className="roomBox scrollbar force-overflow">
+        <div className='row'>
+          <div className='col s12 m12 l12'>
+          {
+            // props.isGameFinished
+          }
+            { isWinner && !isDraw &&
+              <div>
+                <h1 className="playerNameStyle"><i>Congratulations!!, </i>{ username }</h1>
+              </div>
+            }
+            {
+              !isWinner && !isDraw &&
+              <h1 className="playerNameStyle"><i>You Lose!!, </i>{ username }</h1>
+            }
+            
+            {
+              isDraw &&
+              <h1 className="playerNameStyle"><i>Game Draw!!</i></h1>
+            }
+    
+            <hr />
+            <h3 className="playerNameStyle">Score: { props.isGameFinished.score[username] }</h3>
+    
+            <button id="new-game-button" className="btnnya-main linkStyle" onClick={ () => {
+              let audioButton = new Audio();
+              audioButton.src = frogSound
+              audioButton.play()
+              props.history.push('/room');
+            }}>AYO MAIN LAGI</button>
           </div>
-        }
-        {
-          !isWinner && !isDraw &&
-          <h1 className="playerNameStyle"><i>You Lose!!, </i>{ username }</h1>
-        }
-        
-        {
-          isDraw &&
-          <h1 className="playerNameStyle"><i>Game Draw!!</i></h1>
-        }
-
-        <hr />
-        <h3 className="playerNameStyle">Score: { props.isGameFinished.score[username] }</h3>
-
-        <button id="new-game-button" className="btnnya-main linkStyle" onClick={ () => {
-          let audioButton = new Audio();
-          audioButton.src = frogSound
-          audioButton.play()
-          props.history.push('/room');
-        }}>AYO MAIN LAGI</button>
+        </div>      
       </div>
-    </div>      
-  </div>
-  )
+      )
+  }
 }  
 
 export default connect(mapStateToProps, mapDispatchToProps)(Gameover) 
