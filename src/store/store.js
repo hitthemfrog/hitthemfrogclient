d4b5185df7636c86198bc7bc177922899dadb1d4
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-07-31 16:41:27 +0700

    add  redux dev tools support

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import { reducer } from './game.action.reducer.type'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
